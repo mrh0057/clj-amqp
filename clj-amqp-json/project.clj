b3084686cd2adbc076f11eab567e0d66692a915a
Matt Hoyt <mrhoyt4@yahoo.com>
2011-10-13 01:12:08 -0500

    Added a json encode/decoder library that can create consumers or publish a message.

---
+++ clj-amqp-json/project.clj
@@ -0,0 +1,6 @@
+(defproject clj-amqp-json "0.1.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [clj-json "0.4.3"]
+                 [clj-amqp "0.1.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.3.1"]])
---
+++ clj-amqp-json/src/clj_amqp_json/core.clj
@@ -0,0 +1,41 @@
+(ns clj-amqp-json.core
+  (:use clj-json.core)
+  (:require [clj-amqp.core :as amqp]))
+
+(defn encode-body
+  "Will not encode dates.  If you are sending dates convert to long."
+  [body]
+  (.getBytes (generate-string body)))
+
+(defn decode-body
+  "Decodes a body of a message if its json."
+  ([body]
+     (parse-string (new String body) true))
+  ([body char-set]
+     (parse-string (new String body char-set) true)))
+
+(defmacro publish
+  "Used to publish a message thats encode with json"
+  [exchange routing-key body & options]
+  `(clj-amqp.core/publish ~exchange ~routing-key
+                          (clj-amqp.core/encode-body ~body) ~@options))
+
+(defmacro create-consumer
+  "Used to create a consumer function.
+
+body 
+  The body of the expression to execute.
+
+binds 
+  body 
+    The decode json message
+  envelope
+    The envlope
+  properties
+    The properteis for the message."
+  [& body]
+  `(fn [body# ~'envelope ~'properties]
+     (let [~'body (if (:content-type ~'properties)
+                    (clj-amqp-json.core/decode-body body# (:content-type ~'properties))
+                    (clj-amqp-json.core/decode-body body#))]
+       ~@body)))
