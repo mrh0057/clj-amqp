863a11b2157b8555526181fe96217cee4d77cc05
Matt Hoyt <mrhoyt4@yahoo.com>
2011-11-03 04:54:09 -0500

    Added documentation

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[swank-clojure "1.3.1"]
                      [org.clojure/clojure-contrib "1.2.0"]
-                     [org.clojars.weavejester/autodoc "0.9.0"]]
+                     [org.clojars.weavejester/autodoc "0.9.0"]
+                     [codox "0.1.3"]]
   :autodoc {:name "clj-amqp" :page-title "CLJ AMQP API"}
   :warn-on-reflection true)
